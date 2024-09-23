1db4a832718647962e036a4932e5215796c30852
Ivan Ursta <ursta2017@gmail.com>
2024-09-23 17:23:02 +0300

    Demo Plugin afterGetName

---
+++ app/code/Perspective/DemoPlugin/Plugins/Catalog/Model/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Perspective\DemoPlugin\Plugins\Catalog\Model;
+
+class Product
+{
+    public function afterGetName(\Magento\Catalog\Model\Product $product, $name) // ($product($subject), $name($result))
+    {
+        $price = $product->getData('price');
+
+        if ($price < 60) {
+            $name .= " (So cheap)";
+        }
+        else {
+            $name .= " (So expensive)";
+        }
+
+        return $name;
+    }
+}
